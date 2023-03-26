bfe665162ce64562c4ac3d4e47eaba730ff79fdb
ignacio alvarado <nash@ignacios-iMac.local>
2023-03-25 22:34:40 -0300

    Inicio de sesion listo

--- index.js
+++ index.js
@@ -2,6 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const session = require('express-session');
+const PassporLocal = require('passport-local').Strategy;
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser('Mi secreto'));
+app.use(session({
+    secret: 'mi secreto',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new PassporLocal(function(username,password,done){
+    if(username === 'ignacio' && password === '123')
+    return done(null, {id:1,name:'cody'});
+
+    done(null,false);
+}));
+
+passport.serializeUser(function(user,done){
+    done(null, {id:1, name: 'Cody'});
+});
+passport.deserializeUser(function(user,done){
+    done(null, {id:1, name: 'Cody'});
+});
+
+
 app.use(express.static(__dirname + '/public'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 //app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
--- router.js
+++ router.js
@@ -1,20 +1,27 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const conexion = require('./database/db')
 
-router.get('/', (req,res)=>{
+
+
+router.get('/',(req,res,next)=>{
+    if(req.isAuthenticated()) return next();
     
-    /*
-    conexion.query('Select * from usuarios',(error,results) =>{
+    res.redirect('/login');
+} ,(req,res)=>{
+    
+    
+    conexion.query('Select * from cliente',(error,results) =>{
         if(error){
             throw error;
         }else{
-            res.send(results)
+            console.log(results)
         }
     })
-*/
-    res.render('index',{variable:'funcionaaaaaaa'})
+
+    res.render('index')
     
 });
 
@@ -25,5 +32,18 @@ router.get('/edit',(req,res)=>{
 router.get('/about',(req,res)=>{
     res.render('about');
 })
+
+router.get('/community',(req,res)=>{
+    res.render('community');
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+
+router.post('/login',passport.authenticate('local',{
+    successRedirect:'/',
+    failureRedirect: '/login'
+}));
 
 module.exports = router;
